7578e87cf131e041bf550105e650716127246165
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-09-15 17:27:03 +0900

    FINALLY FUCKING TRACKED A PLANE LIVE lol

--- public/scripts/scripts.bundle.js
+++ public/scripts/scripts.bundle.js
@@ -78,30 +78,20 @@
 	google.load("visualization", "1", {packages:["map"]});
 
 
+	// data: { 'ident': 'SIA285', 'departureTime': '1473945000' },
+	// data: { 'ident': 'ANZ281', 'departureTime': '1473901980' },
+
 	$(document).ready(function() {
 	    $(document).on('click', '#tracker-button', function() {
 	      console.log('test')
 	    $.ajax({
 	        type: 'GET',
-	        url: fxml_url + 'FlightInfoEx',
-	        data: { 'ident': $('#ident_text').val(), 'howMany': 10, 'offset': 0 },
+	        url: fxml_url + 'GetLastTrack',
+	        data: { 'ident': "ANZ281"},
+	        // data: { 'faFlightID': "ANZ281-1473730220-airline-0890"},
+	        // data: { 'ident': 'SIA285-1473744949-airline-0114', 'departureTime': '1473945000' },
 	        success : function(result) {
-	          console.log('yes')
-	            if (result.error) {
-	                alert('Failed to fetch flight: ' + result.error);
-	                return;
-	            }
-	            for (flight of result.FlightInfoExResult.flights) {
-	                if (flight.actualdeparturetime > 0) {
-	                    // display some textual details about the flight.
-	                    console.log('Flight ' + flight.ident + ' from ' + flight.origin + ' to ' + flight.destination);
-
-	                    // display the route on a map.
-	                    fetchAndRenderRoute(flight.faFlightID);
-	                    return;
-	                }
-	            }
-	            alert('Did not find any useful flights');
+	          console.log(result)
 	        },
 	        error: function(data, text) { alert('Failed to fetch flight: ' + data); },
 	        dataType: 'jsonp',
@@ -110,45 +100,6 @@
 	        });
 	    });
 	});
-
-
-	// Fetch the planned route for a specified flight_id.
-	function fetchAndRenderRoute(flight_id) {
-	    $.ajax({
-	       type: 'GET',
-	       url: fxml_url + 'DecodeFlightRoute', 
-	       data: { 'faFlightID': flight_id },
-	       success : function(result) {
-	           if (result.error) {
-	               alert('Failed to decode route: ' + result.error);
-	               return;
-	           }
-
-	           // Initialize a data table using the Google API.
-	           var table = new google.visualization.DataTable();
-	           table.addColumn('number', 'Lat');
-	           table.addColumn('number', 'Lon');
-	           table.addColumn('string', 'Name');
-
-	           // Insert all of the points into the data table.
-	           var points = result.DecodeFlightRouteResult.data;
-	           table.addRows(points.length);
-	           for (rowid = 0; rowid < points.length; rowid++) {
-	                table.setCell(rowid, 0, points[rowid].latitude);
-	                table.setCell(rowid, 1, points[rowid].longitude);
-	                table.setCell(rowid, 2, points[rowid].name + ' (' + points[rowid].type + ')' );
-	           }
-
-	           // Render the data table into a map using Google Maps API.
-	           var map = new google.visualization.Map(document.getElementById('map_div'));
-	           map.draw(table, {showTip: true, showLine: true, lineWidth: 3, lineColor: '#009900'});
-	       },
-	       error: function(data, text) { alert('Failed to decode route: ' + data); },
-	       dataType: 'jsonp',
-	       jsonp: 'jsonp_callback',
-	       xhrFields: { withCredentials: true }
-	   });
-	}
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(5)))
 
 /***/ },
